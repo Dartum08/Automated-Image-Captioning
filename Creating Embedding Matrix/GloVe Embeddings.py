a0266a30c30359f4d4ff9b80e78cdd9142101632
Arun Rajora <43806173+Dartum08@users.noreply.github.com>
2020-06-22 15:08:03 +0530

    Add files via upload

---
+++ Creating Embedding Matrix/GloVe Embeddings.py
@@ -0,0 +1,26 @@
+# Load Glove vectors
+glove_dir = "/content/drive/My Drive/Image Captioning Data/GloVe"
+embeddings_index = {} # empty dictionary
+f = open(os.path.join(glove_dir, 'glove.6B.200d.txt'), encoding="utf-8")
+
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Found %s word vectors.' % len(embeddings_index))
+#Found 400000 word vectors.
+
+embedding_dim = 200
+
+# Get 200-dim dense vector for each of the 10000 words in out vocabulary
+embedding_matrix = np.zeros((vocab_size, embedding_dim))
+
+for word, i in word_to_ix.items():
+    #if i < max_words:
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # Words not found in the embedding index will be all zeros
+        embedding_matrix[i] = embedding_vector
