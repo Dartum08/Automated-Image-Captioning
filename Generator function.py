d83e2a39a99990f57a2aa8f8d1558c4b0deeeb9f
Arun Rajora <43806173+Dartum08@users.noreply.github.com>
2020-06-22 14:52:17 +0530

    Add files via upload

---
+++ Generator function.py
@@ -0,0 +1,30 @@
+# data generator, intended to be used in a call to model.fit_generator()
+def data_generator(descriptions, photos, wordtoix, max_length, num_photos_per_batch):
+    X1, X2, y = list(), list(), list()
+    n=0
+    # loop for ever over images
+    while 1:
+        for key, desc_list in descriptions.items():
+            n+=1
+            # retrieve the photo feature
+            photo = photos[key]
+            for desc in desc_list:
+                # encode the sequence
+                seq = [wordtoix[word] for word in desc.split(' ') if word in wordtoix]
+                # split one sequence into multiple X, y pairs
+                for i in range(1, len(seq)):
+                    # split into input and output pair
+                    in_seq, out_seq = seq[:i], seq[i]
+                    # pad input sequence
+                    in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+                    # encode output sequence
+                    out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+                    # store
+                    X1.append(photo)
+                    X2.append(in_seq)
+                    y.append(out_seq)
+            # yield the batch data
+            if n==num_photos_per_batch:
+                yield [[array(X1), array(X2)], array(y)]
+                X1, X2, y = list(), list(), list()
+                n=0
